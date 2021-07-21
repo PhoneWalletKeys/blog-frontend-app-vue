625af4e7ea0b10b1cef690f8984efbfbba920966
PhoneWalletKeys <rsvetter91@gmail.com>
2021-07-21 12:33:44 -0700

    added auth and signup pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+import PostsIndex from "../views/PostsIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +23,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  { path: "/test", name: "Test", component: Test },
+  { path: "/signup", name: "Signup", component: Signup },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/logout", name: "Logout", component: Logout },
+  { path: "/posts", name: "PostsIndex", component: PostsIndex },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Testing.....1,2,3</h1>
+  </div>
+</template>
